04ad03305c652b63f3ac7eaba0b7197d270572c5
Nick Lewis <nicklewis16@uchicago.edu>
2024-03-07 04:35:21 -0600

    finish done

--- dodo.py
+++ dodo.py
@@ -51,256 +51,256 @@ def copy_notebook_to_folder(notebook_stem, origin_folder, destination_folder):
     return command
 
     
-def task_pull_ken_french_data():
-    """Pull CRSP/Compustat data from WRDS and save to disk
-    """
-    file_dep = [
-        "./src/config.py", 
-        "./src/pull_test_data.py"
-        ]
-    targets = [
-        Path(DATA_DIR) / "famafrench" / file for file in 
-        [
-            ## src/pull_test_data.py
-            "5_Industry_Portfolios_daily.xlsx",
-            "5_Industry_Portfolios_Wout_Div.xlsx",
-            "5_Industry_Portfolios.xlsx",
-            "6_Portfolios_ME_CFP_2x3_Wout_Div.xlsx", 
-            "6_Portfolios_ME_CFP_2x3.xlsx",
-            "6_Portfolios_ME_DP_2x3_Wout_Div.xlsx",
-            "6_Portfolios_ME_DP_2x3.xlsx",
-            "6_Portfolios_ME_EP_2x3_Wout_Div.xlsx",
-            "6_Portfolios_ME_EP_2x3.xlsx",
-            "25_Portfolios_OP_INV_5x5_daily.xlsx",
-            "25_Portfolios_OP_INV_5x5_Wout_Div.xlsx",
-            "25_Portfolios_OP_INV_5x5.xlsx",
-            "49_Industry_Portfolios_daily.xlsx",
-            "49_Industry_Portfolios_Wout_Div.xlsx",
-            "49_Industry_Portfolios.xlsx",
-            "Portfolios_Formed_on_CF-P_Wout_Div.xlsx",
-            "Portfolios_Formed_on_CF-P.xlsx",
-            "Portfolios_Formed_on_D-P_Wout_Div.xlsx",
-            "Portfolios_Formed_on_D-P.xlsx",
-            "Portfolios_Formed_on_E-P_Wout_Div.xlsx",
-            "Portfolios_Formed_on_E-P.xlsx"
-        ]
-    ]
+# def task_pull_ken_french_data():
+#     """Pull CRSP/Compustat data from WRDS and save to disk
+#     """
+#     file_dep = [
+#         "./src/config.py", 
+#         "./src/pull_test_data.py"
+#         ]
+#     targets = [
+#         Path(DATA_DIR) / "famafrench" / file for file in 
+#         [
+#             ## src/pull_test_data.py
+#             "5_Industry_Portfolios_daily.xlsx",
+#             "5_Industry_Portfolios_Wout_Div.xlsx",
+#             "5_Industry_Portfolios.xlsx",
+#             "6_Portfolios_ME_CFP_2x3_Wout_Div.xlsx", 
+#             "6_Portfolios_ME_CFP_2x3.xlsx",
+#             "6_Portfolios_ME_DP_2x3_Wout_Div.xlsx",
+#             "6_Portfolios_ME_DP_2x3.xlsx",
+#             "6_Portfolios_ME_EP_2x3_Wout_Div.xlsx",
+#             "6_Portfolios_ME_EP_2x3.xlsx",
+#             "25_Portfolios_OP_INV_5x5_daily.xlsx",
+#             "25_Portfolios_OP_INV_5x5_Wout_Div.xlsx",
+#             "25_Portfolios_OP_INV_5x5.xlsx",
+#             "49_Industry_Portfolios_daily.xlsx",
+#             "49_Industry_Portfolios_Wout_Div.xlsx",
+#             "49_Industry_Portfolios.xlsx",
+#             "Portfolios_Formed_on_CF-P_Wout_Div.xlsx",
+#             "Portfolios_Formed_on_CF-P.xlsx",
+#             "Portfolios_Formed_on_D-P_Wout_Div.xlsx",
+#             "Portfolios_Formed_on_D-P.xlsx",
+#             "Portfolios_Formed_on_E-P_Wout_Div.xlsx",
+#             "Portfolios_Formed_on_E-P.xlsx"
+#         ]
+#     ]
 
-    return {
-        "actions": [
-            "ipython src/config.py",
-            "ipython src/pull_test_data.py",
-        ],
-        "targets": targets,
-        "file_dep": file_dep,
-        "clean": True,
-        "verbosity": 2,
-    }
-
-
-def task_pull_CRSP_Compustat():
-    """Pull CRSP/Compustat data from WRDS and save to disk
-    """
-    file_dep = [
-        "./src/config.py", 
-        "./src/load_CRSP_stock.py",
-        "./src/load_CRSP_Compustat.py",
-        "./src/load_CRSP_stock_v2.py",
-        "./src/load_CRSP_Compustat_v2.py",
-        ]
-    targets = [
-        Path(DATA_DIR) / "pulled" / file for file in 
-        [
-            ## src/load_CRSP_Compustat.py
-            "Compustat.parquet",
-            "CRSP_stock.parquet",
-            "CRSP_Comp_Link_Table.parquet",
-            ## src/load_CRSP_stock.py
-            "CRSP_MSF_INDEX_INPUTS.parquet", 
-            "CRSP_MSIX.parquet", 
-            ## src/load_CRSP_Compustat_v2.py
-            "v2/Compustat.parquet",
-            "v2/CRSP_stock.parquet",
-            "v2/CRSP_Comp_Link_Table.parquet",
-            ## src/load_CRSP_stock_v2.py
-            "v2/CRSP_MSF_INDEX_INPUTS.parquet", 
-            "v2/CRSP_MSIX.parquet", 
-        ]
-    ]
-
-    return {
-        "actions": [
-            "ipython src/config.py",
-            "ipython src/load_CRSP_Compustat.py",
-            "ipython src/load_CRSP_stock.py",
-            "ipython src/load_CRSP_Compustat_v2.py",
-            "ipython src/load_CRSP_stock_v2.py",
-        ],
-        "targets": targets,
-        "file_dep": file_dep,
-        "clean": True,
-        "verbosity": 2, # Print everything immediately. This is important in
-        # case WRDS asks for credentials.
-    }
+#     return {
+#         "actions": [
+#             "ipython src/config.py",
+#             "ipython src/pull_test_data.py",
+#         ],
+#         "targets": targets,
+#         "file_dep": file_dep,
+#         "clean": True,
+#         "verbosity": 2,
+#     }
 
 
+# def task_pull_CRSP_Compustat():
+#     """Pull CRSP/Compustat data from WRDS and save to disk
+#     """
+#     file_dep = [
+#         "./src/config.py", 
+#         "./src/load_CRSP_stock.py",
+#         "./src/load_CRSP_Compustat.py",
+#         "./src/load_CRSP_stock_v2.py",
+#         "./src/load_CRSP_Compustat_v2.py",
+#         ]
+#     targets = [
+#         Path(DATA_DIR) / "pulled" / file for file in 
+#         [
+#             ## src/load_CRSP_Compustat.py
+#             "Compustat.parquet",
+#             "CRSP_stock.parquet",
+#             "CRSP_Comp_Link_Table.parquet",
+#             ## src/load_CRSP_stock.py
+#             "CRSP_MSF_INDEX_INPUTS.parquet", 
+#             "CRSP_MSIX.parquet", 
+#             ## src/load_CRSP_Compustat_v2.py
+#             "v2/Compustat.parquet",
+#             "v2/CRSP_stock.parquet",
+#             "v2/CRSP_Comp_Link_Table.parquet",
+#             ## src/load_CRSP_stock_v2.py
+#             "v2/CRSP_MSF_INDEX_INPUTS.parquet", 
+#             "v2/CRSP_MSIX.parquet", 
+#         ]
+#     ]
 
-def task_calc_industries():
-    """calculates the industry portfolios
-    """
-    file_dep = [
-        "./src/config.py", 
-        "./src/load_CRSP_stock.py",
-        "./src/load_CRSP_Compustat.py",
-        ]
-    targets = [
-        Path(DATA_DIR) / "manual" / file for file in 
-        [
-            ## src/calc_industry_portfolios.py
-            "5industry_portfolios.xlsx",
-            "49industry_portfolios.xlsx"
-        ]
-    ]
-
-    return {
-        "actions": [
-            "ipython src/calc_industry_portfolios.py",
-        ],
-        "targets": targets,
-        "file_dep": file_dep,
-        "clean": True,
-        "verbosity": 2, 
-    }
-
-def task_analyze_industries():
-    """calculates the industry portfolios
-    """
-    file_dep = [
-        "./src/calc_industry_portfolios.py",
-        ]
-    targets = [
-        Path(OUTPUT_DIR) / file for file in 
-        [
-            ## src/calc_industry_portfolios.py
-            "sec_per_ind_5.png",
-            "actual_sec_per_ind_5.png",
-            "summary_stats_5ind.tex",
-            'actual_summary_stats_5ind.tex',
-        ]
-    ]
-
-    return {
-        "actions": [
-            "ipython src/analyze_industry_portfolios.py",
-        ],
-        "targets": targets,
-        "file_dep": file_dep,
-        "clean": True,
-        "verbosity": 2, 
-    }
+#     return {
+#         "actions": [
+#             "ipython src/config.py",
+#             "ipython src/load_CRSP_Compustat.py",
+#             "ipython src/load_CRSP_stock.py",
+#             "ipython src/load_CRSP_Compustat_v2.py",
+#             "ipython src/load_CRSP_stock_v2.py",
+#         ],
+#         "targets": targets,
+#         "file_dep": file_dep,
+#         "clean": True,
+#         "verbosity": 2, # Print everything immediately. This is important in
+#         # case WRDS asks for credentials.
+#     }
 
 
 
-def task_calc_inv_op():
-    """calculates the inv op portfolios
-    """
-    file_dep = [
-        "./src/config.py", 
-        "./src/load_CRSP_stock.py",
-        "./src/load_CRSP_Compustat.py",
-        ]
-    targets = [
-        Path(DATA_DIR) / "manual" / file for file in 
-        [
-            ## src/calc_op_inv_portfolios.py
-            "5x5_OP_INV_portfolios.xlsx",
-        ]
-    ]
+# def task_calc_industries():
+#     """calculates the industry portfolios
+#     """
+#     file_dep = [
+#         "./src/config.py", 
+#         "./src/load_CRSP_stock.py",
+#         "./src/load_CRSP_Compustat.py",
+#         ]
+#     targets = [
+#         Path(DATA_DIR) / "manual" / file for file in 
+#         [
+#             ## src/calc_industry_portfolios.py
+#             "5industry_portfolios.xlsx",
+#             "49industry_portfolios.xlsx"
+#         ]
+#     ]
 
-    return {
-        "actions": [
-            "ipython src/calc_op_inv_portfolios.py",
-        ],
-        "targets": targets,
-        "file_dep": file_dep,
-        "clean": True,
-        "verbosity": 2, 
-    }
+#     return {
+#         "actions": [
+#             "ipython src/calc_industry_portfolios.py",
+#         ],
+#         "targets": targets,
+#         "file_dep": file_dep,
+#         "clean": True,
+#         "verbosity": 2, 
+#     }
+
+# def task_analyze_industries():
+#     """calculates the industry portfolios
+#     """
+#     file_dep = [
+#         "./src/calc_industry_portfolios.py",
+#         ]
+#     targets = [
+#         Path(OUTPUT_DIR) / file for file in 
+#         [
+#             ## src/calc_industry_portfolios.py
+#             "sec_per_ind_5.png",
+#             "actual_sec_per_ind_5.png",
+#             "summary_stats_5ind.tex",
+#             'actual_summary_stats_5ind.tex',
+#         ]
+#     ]
+
+#     return {
+#         "actions": [
+#             "ipython src/analyze_industry_portfolios.py",
+#         ],
+#         "targets": targets,
+#         "file_dep": file_dep,
+#         "clean": True,
+#         "verbosity": 2, 
+#     }
+
+
+
+# def task_calc_inv_op():
+#     """calculates the inv op portfolios
+#     """
+#     file_dep = [
+#         "./src/config.py", 
+#         "./src/load_CRSP_stock.py",
+#         "./src/load_CRSP_Compustat.py",
+#         ]
+#     targets = [
+#         Path(DATA_DIR) / "manual" / file for file in 
+#         [
+#             ## src/calc_op_inv_portfolios.py
+#             "5x5_OP_INV_portfolios.xlsx",
+#         ]
+#     ]
+
+#     return {
+#         "actions": [
+#             "ipython src/calc_op_inv_portfolios.py",
+#         ],
+#         "targets": targets,
+#         "file_dep": file_dep,
+#         "clean": True,
+#         "verbosity": 2, 
+#     }
     
-def task_analyze_op_inv():
-    """calculates the inv_op portfolios
-    """
-    file_dep = [
-        "./src/calc_op_inv_portfolios.py",
-        ]
-    targets = [
-        Path(OUTPUT_DIR) / file for file in 
-        [
-            ## src/analyze_OP_INV_portfolios.py
-            'output/opinv_mixed.png',
-            'output/opinv_same.png'
-        ]
-    ]
+# def task_analyze_op_inv():
+#     """calculates the inv_op portfolios
+#     """
+#     file_dep = [
+#         "./src/calc_op_inv_portfolios.py",
+#         ]
+#     targets = [
+#         Path(OUTPUT_DIR) / file for file in 
+#         [
+#             ## src/analyze_OP_INV_portfolios.py
+#             'output/opinv_mixed.png',
+#             'output/opinv_same.png'
+#         ]
+#     ]
 
-    return {
-        "actions": [
-            "ipython src/analyze_OP_INV_portfolios.py",
-        ],
-        "targets": targets,
-        "file_dep": file_dep,
-        "clean": True,
-        "verbosity": 2, 
-    }
+#     return {
+#         "actions": [
+#             "ipython src/analyze_OP_INV_portfolios.py",
+#         ],
+#         "targets": targets,
+#         "file_dep": file_dep,
+#         "clean": True,
+#         "verbosity": 2, 
+#     }
     
-def task_calc_univ_portfolios_temp():
-    """calculates the univariate portfolios
-    """
-    file_dep = [
-        "./src/config.py", 
-        "./src/load_CRSP_stock_v2.py",
-        "./src/load_CRSP_Compustat_v2.py",
-        ]
-    targets = [
-        Path(DATA_DIR) / "manual" / file for file in 
-        [
-            "portfolio_metrics.xlsx",
-        ]
-    ]
+# def task_calc_univ_portfolios_temp():
+#     """calculates the univariate portfolios
+#     """
+#     file_dep = [
+#         "./src/config.py", 
+#         "./src/load_CRSP_stock_v2.py",
+#         "./src/load_CRSP_Compustat_v2.py",
+#         ]
+#     targets = [
+#         Path(DATA_DIR) / "manual" / file for file in 
+#         [
+#             "portfolio_metrics.xlsx",
+#         ]
+#     ]
 
-    return {
-        "actions": [
-            "ipython src/calc_univariate_portfolios.py",
-        ],
-        "targets": targets,
-        "file_dep": file_dep,
-        "clean": True,
-        "verbosity": 2, 
-    }
+#     return {
+#         "actions": [
+#             "ipython src/calc_univariate_portfolios.py",
+#         ],
+#         "targets": targets,
+#         "file_dep": file_dep,
+#         "clean": True,
+#         "verbosity": 2, 
+#     }
 
 
-def task_calc_univ_portfolios_temp():
-    """calculates the univariate portfolios
-    """
-    file_dep = [
-        "./src/calc_univariate_portfolios.py"
-        ]
-    targets = [
-        Path(OUTPUT_DIR) / file for file in 
-        [
-            ## src/calc_op_inv_portfolios.py
-            "portfolio_metrics_final.xlsx",
-        ]
-    ]
+# def task_calc_univ_portfolios_temp():
+#     """calculates the univariate portfolios
+#     """
+#     file_dep = [
+#         "./src/calc_univariate_portfolios.py"
+#         ]
+#     targets = [
+#         Path(OUTPUT_DIR) / file for file in 
+#         [
+#             ## src/calc_op_inv_portfolios.py
+#             "portfolio_metrics_final.xlsx",
+#         ]
+#     ]
 
-    return {
-        "actions": [
-            "ipython src/univ_ptf_output.py",
-        ],
-        "targets": targets,
-        "file_dep": file_dep,
-        "clean": True,
-        "verbosity": 2, 
-    }
+#     return {
+#         "actions": [
+#             "ipython src/univ_ptf_output.py",
+#         ],
+#         "targets": targets,
+#         "file_dep": file_dep,
+#         "clean": True,
+#         "verbosity": 2, 
+#     }
 
 
 def task_compile_latex():
