ded7886ecfbacc3aa9f959a77cbac25ca6a45824
rgutgeld <gutgeld@uchicago.edu>
2024-03-07 01:56:33 -0600

    update

--- src/load_CRSP_Compustat_v2.py
+++ src/load_CRSP_Compustat_v2.py
@@ -72,7 +72,7 @@ def pull_compustat(wrds_username=WRDS_USERNAME):
     sql_query = """
         SELECT 
             gvkey, datadate, at, sale, cogs, xsga, xint, pstkl, txditc,
-            pstkrv, seq, pstk, ni, sich, op, inv
+            pstkrv, seq, pstk, ni, sich, dp, ebit
         FROM 
             comp.funda
         WHERE 
